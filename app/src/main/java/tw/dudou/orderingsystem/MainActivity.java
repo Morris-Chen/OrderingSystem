ccd3ffcce5aeb5d8eacee18002fd32d123fe1909
Chien-To Chen <finalfire.to@gmail.com>
2015-06-04 20:39:27 +0800

    Add clear button and "Enter" key press processing
    add Send2() to clear the textfield
    add setOnKeyLestiner with anonymous class and process when user click
    Enter.

--- app/src/main/java/tw/dudou/orderingsystem/MainActivity.java
+++ app/src/main/java/tw/dudou/orderingsystem/MainActivity.java
@@ -2,6 +2,7 @@ package tw.dudou.orderingsystem;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,9 +20,24 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        inputEditText = (EditText) findViewById(R.id.editText);
-        sendButton = (Button) findViewById(R.id.button);
 
+        inputEditText = (EditText) findViewById(R.id.editText);
+        inputEditText.setOnKeyListener(new View.OnKeyListener() {
+            // onKeyListener's reference keyCode is reference in KeyEvent.KEYCODE_ENTER
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                    if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                        sendTextToToast();
+                        return true; // if you don't want to continue the key press to continue processing, return true.
+                    }
+                }
+                return false; //if you still want Enter to show, return false and let the key press continue to process.
+            }
+        });
+
+
+        sendButton = (Button) findViewById(R.id.button);
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,6 +52,15 @@ public class MainActivity extends ActionBarActivity {
         Toast.makeText(this,text,Toast.LENGTH_SHORT).show();
 
     }
+
+    public void send2(View view){
+        // if declare to xml: onClick, the function if and on if follows:
+        // 1. it is a public function
+        // 2. it has only one reference: View
+        inputEditText.setText("");
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
